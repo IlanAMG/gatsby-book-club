9ceef97070de47396601a31dcc7a91761ba3773f
Ilan Amzallag <59542957+IlanAMG@users.noreply.github.com>
2020-05-15 22:28:11 +0200

    add styled-component

---
+++ src/components/StyledBookItem.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledBookItem = styled.section`
+    border: 5px solid red;
+
+    h2 {
+        small {
+            font-weight: normal;
+        }
+    }
+`
+
+export default StyledBookItem
--- src/templates/bookTemplate.js
+++ src/templates/bookTemplate.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 import Layout from '../components/layout';
-import styled from 'styled-components';
+import StyledBookItem from '../components/StyledBookItem';
 
 const bookTemplate = (props) => {
     return (
         <Layout>
-            <section>
+            <StyledBookItem>
                 <h2>
                     {props.pageContext.title} - <small>{props.pageContext.author.name}</small>
                 </h2>
                 <p>
                     {props.pageContext.summary}
                 </p>
-            </section>
+            </StyledBookItem>
         </Layout>
     )
 }
